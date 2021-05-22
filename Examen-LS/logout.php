d17367181192fd0b2f236f7e0f38f8cf2cc6b57d
Alexandru Ostafi <61746596+alexandruostafi@users.noreply.github.com>
2021-05-22 14:22:26 +0300

    Update logout.php

--- Examen-LS/logout.php
+++ Examen-LS/logout.php
@@ -5,4 +5,5 @@ session_start();
 // Destroying the session clears the $_SESSION variable, thus "logging" the user
 // out. This also happens automatically when the browser is closed
 session_destroy();
+header('Location: login.php');
 ?>
